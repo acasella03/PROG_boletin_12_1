8dde1f0f290a1b3d52e3667853ff3703afa4dfa2
Angi Casella <acasella@danielcastelao.org>
2022-11-15 20:16:58 +0100

    corrección del código

--- src/prog_boletin_12_1/Numero.java
+++ src/prog_boletin_12_1/Numero.java
@@ -4,36 +4,32 @@ import javax.swing.JOptionPane;
 
 public class Numero {
 
-    boolean sw = true;
-    int numero;
+    int numeroSecreto;
+    int cantidadIntentos;
+    int intento;
 
     public void pedirNumero() {
         do {
-            numero = pedirEntero();
+            numeroSecreto = pedirEntero("Jugador 1: Teclea el número secreto");
+            cantidadIntentos = pedirEntero("Jugador 1: Asigna el número de intentos que va a tener el Jugador 2");
+        } while (numeroSecreto <= 0 || numeroSecreto > 50);
+        for (int i = 1; i <= cantidadIntentos; i++) {
+            intento = pedirEntero("Juagador 2: Teclea y adivina el número del Jugador 1");
+            if (intento == numeroSecreto) {
+                JOptionPane.showMessageDialog(null, "¡Adivinaste!");
+                break;
+            } else if (i<cantidadIntentos && intento>numeroSecreto){
+                JOptionPane.showMessageDialog(null, "El número es menor\nInténtalo otra vez");
+            }else if (intento<numeroSecreto){
+                JOptionPane.showMessageDialog(null, "El número es mayor\nInténtalo otra vez");
+            }else
+                JOptionPane.showMessageDialog(null, "Fin del Juego\nSe te acabaron los intentos");
 
-            for (int i = 1; i < 6 ; i++) {
-                numero = pedirEntero();
-                if (numero <= 1) {
-                    if (numero <= 50) {
-                        if (sw != true) {
-                            JOptionPane.showMessageDialog(null, "¡Adivinaste!");
-                            sw = false;
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Inténtalo otra vez");
-                        }
-                        sw = true;
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Introduce un número menor que 50");
-                    }
-                } else {
-                    JOptionPane.showMessageDialog(null, "Introduce un número mayor que 1");
-                }
-            }
-        } while (numero <= 0 || numero > 50);
+        }
     }
 
-    public static int pedirEntero() {
-        return Integer.parseInt(JOptionPane.showInputDialog("Teclea número"));
+    public static int pedirEntero(String mensaje) {
+        return Integer.parseInt(JOptionPane.showInputDialog(mensaje));
     }
 
 }
